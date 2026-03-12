635b1c6370bdc88c1d14fd72efc356b59dc145a8
tmarques2 <thainaramarques258@gmail.com>
2026-03-12 10:16:15 -0300

    aula5

--- aula5/ex1.dart
+++ aula5/ex1.dart
@@ -3,31 +3,6 @@ Aula 05 - Exemplo polimorfismo
 05/03/2026
 */
 
-<<<<<<< HEAD
-// cria a classe mae Animal
-class Animal {
-  void fazerSom() {
-    print("O animal faz um som!");
-  }
-}
-
-class Cachorro extends Animal {
-  @override
-  void fazerSom() {
-    print("O cachorro late auau");
-  }
-}
-
-class Gato extends Animal {
-  @override
-  void fazerSom() {
-    print("O gato mia miau");
-  }
-}
-
-
-void main(){
-=======
 
 // Cria a classe mae Animal
 class Animal{
@@ -56,14 +31,9 @@ class Cachorro extends Animal{
 
 
  void main(){
->>>>>>> 1e3f306c5a9fe8367400afe70047031b43cd1fa7
   Animal meuAnimal = Cachorro();
   meuAnimal.fazerSom();
 
   Animal meuAnimal1 = Gato();
   meuAnimal1.fazerSom();
-<<<<<<< HEAD
 }
-=======
- }
->>>>>>> 1e3f306c5a9fe8367400afe70047031b43cd1fa7
